58f338e3846b026dca995494999b23a16705c3ec
poojasingh1002 <ptassingh@gmail.com>
2024-09-11 13:59:49 +0530

    XYZ

--- src/Routing.js
+++ src/Routing.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Routes ,Route} from 'react-router-dom';
+import { Routes ,Route, Link} from 'react-router-dom';
 import Home from './Home';
 import Electronics from './Electronics';
 import Jewellery from './Jewellery';
@@ -9,23 +9,23 @@ import Mencloth from './Mencloth';
 import Womencloth from './Womencloth';
 
 function Routing() {
-  
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/mobile">Mobile</Nav.Link>
-            <Nav.Link href="/laptop">Laptop</Nav.Link>
-            <Nav.Link href="/electronics">Electronics</Nav.Link>
-            <Nav.Link href="/jewellery">Jewellery</Nav.Link>
-            <Nav.Link href="/men Cloth">Men Cloth</Nav.Link>
-            <Nav.Link href="/women cloth">Women Cloth</Nav.Link>
-            <Nav.Link href="/help">Help</Nav.Link>
-            <Nav.Link><button>Login</button></Nav.Link>
-            <Nav.Link><button>Register</button></Nav.Link>
-          </Nav>
+            <Link to="/">Home</Link>
+            <Link to="/mobile">Mobile</Link>
+            <Link to="/laptop">Laptop</Link>
+            <Link to="/electronics">Electronics</Link>
+            <Link to="/jewellery">Jewellery</Link>
+            <Link to="/men Cloth">Men Cloth</Link>
+            <Link to="/women cloth">Women Cloth</Link>
+            <Link to="/help">Help</Link>
+            <Link><button>Login</button></Link>
+            <Link><button>Register</button></Link>
+              
+        
         </Container>
       </Navbar>
       <Routes>
